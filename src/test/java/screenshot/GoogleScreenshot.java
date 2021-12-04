3a211f6ccba3302846863fab950af99a547db099
mailtoadinesh <mailtoadinesh@gmail.com>
2021-12-04 19:11:14 +0530

    Google Screenshot

---
+++ src/test/java/screenshot/GoogleScreenshot.java
@@ -0,0 +1,37 @@
+package screenshot;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class GoogleScreenshot {
+	
+	@Test
+	public void TakeScreenshot() throws IOException{
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd = new ChromeDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com/");
+		WebElement input=wd.findElement(By.name("q"));
+		input.sendKeys("Chennai");
+		input.sendKeys(Keys.ENTER);
+		
+		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src, new File("C:\\Users\\Dinesh\\Desktop\\SeleniumTesting\\TestNGGitHubScreenshot\\Folder\\Google.png"));
+
+	}
+		
+}
